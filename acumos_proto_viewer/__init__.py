b664f82762261fe1982cf77978081e3a978b9aa3
Lott, Christopher (cl778h) <cl778h@att.com>
2018-06-11 16:27:25 -0400

    Add docs, repair minor typo in key
    Change apv_recieved to apv_received.
    Bump version to 1.51.
    
    Change-Id: I3378b64c5dbf9b13a7e0900f38f4971a7705b7d6
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- acumos_proto_viewer/__init__.py
+++ acumos_proto_viewer/__init__.py
@@ -3,6 +3,8 @@ import logging
 
 def get_module_logger(mod_name):
     """
+    Gets a logger for the named module.
+    Configures a stream handler and formatter; uses level DEBUG.
     To use this, do logger = get_module_logger(__name__)
     """
     logger = logging.getLogger(mod_name)
