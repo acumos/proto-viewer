0069bf5f7e22667ddc704d4271e3328d81f7cbeb
Lott, Christopher (cl778h) <cl778h@att.com>
2018-09-26 10:50:01 -0400

    Extend model-id generation method
    The proto file name no longer has a globally unique string in it
    so use the full URL, not just the last path component.
    
    Change-Id: I439785a30a4f9f7c363b7ecd0723fe1b6361d3c0
    Issue-ID: ACUMOS-1793
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@
 
 from requests.exceptions import HTTPError
 import pytest
-import os
 from acumos_proto_viewer.utils import load_proto
 from acumos_proto_viewer import data
 
@@ -18,96 +17,36 @@ class FakeResponse():
             raise HTTPError(response=FakeResponse(404, ""))
 
 
-test_proto = """
-syntax = "proto3";
-
-package mygreatpackage;
-
-message Data1 {
-    double a = 1;
-    float b = 2;
-    int32 c = 3;
-    int64 d = 4;
-    bool e = 5;
-    string f = 6;
-    bytes g = 7;
-}
-
-message Data2 {
-    Data1 a = 1;
-    map<string, Data1> b = 2;
-    map<string, int32> c = 3;
-    repeated Data1 d = 4;
-    repeated int32 e = 5;
-}
-"""
-
-
-test_proto_with_arrays = """
-syntax = "proto3";
-package YKhGXjKWHYsPwKJFfEPnmoHOkDkPKBxX;
-
-service Model {
-  rpc classify (ImageTagSet) returns (ImageTagSet);
-}
-
-message ImageTagSet {
-  repeated int64 image = 1;
-  repeated string tag = 2;
-  repeated double score = 3;
-}
-"""
-
-
 @pytest.fixture
-def monkeyed_requests_get():
+def monkeyed_requests_get(test_proto_url, test_proto_txt,
+                          test_proto_with_arrays_url, test_proto_with_arrays_txt,
+                          test_probe_fake_schema_url, test_probe_fake_schema_txt):
     def mrg(url):
-        if url == "http://myserver.com/fakemodelid/1.0.0/fakemodelid-1.0.0-proto":
-            return FakeResponse(status_code=200,
-                                text=test_proto)
-        if url == "http://myserver.com/fakemodelidwitharrays/1.0.0/fakemodelidwitharrays-1.0.0-proto":
-            return FakeResponse(status_code=200,
-                                text=test_proto_with_arrays)
+        if url == test_proto_url:
+            return FakeResponse(status_code=200, text=test_proto_txt)
+        if url == test_proto_with_arrays_url:
+            return FakeResponse(status_code=200, text=test_proto_with_arrays_txt)
         if url == "http://myserver.com/emptyinside":
-            return FakeResponse(status_code=404,
-                                text="")
-        if url == "http://myserver.com/probe_testschema/1.0.0/probe_testschema-1.0.0":
-            fake_schema = """
-            {
-                "$schema": "http://json-schema.org/draft-04/schema#",
-                "properties": {
-                  "value": {
-                    "type": "number",
-                    "description": ""
-                  }
-                },
-                "required": ["value"],
-                "type": "object"
-              }
-            """
-            return FakeResponse(status_code=200, text=fake_schema)
+            return FakeResponse(status_code=404, text="")
+        if url == test_probe_fake_schema_url:
+            return FakeResponse(status_code=200, text=test_probe_fake_schema_txt)
 
     return mrg
 
 
 @pytest.fixture
-def cleanuptmp():
+def cleanuptmp(test_proto_mid, test_proto_with_arrays_mid):
     def _dt():
-        os.remove("/tmp/protofiles/fakemodelid_100_proto_pb2.py")
-        os.remove("/tmp/protofiles/fakemodelid_100_proto.proto")
-        os.remove("/tmp/protofiles/fakemodelidwitharrays_100_proto_pb2.py")
-        os.remove("/tmp/protofiles/fakemodelidwitharrays_100_proto.proto")
-        del data.proto_data_structure["fakemodelid_100_proto"]
-        del data.proto_data_structure["fakemodelidwitharrays_100_proto"]
-        assert('fakemodelidwitharrays_100_proto' not in data.list_known_protobufs())
-        assert('fakemodelid_100_proto' not in data.list_known_protobufs())
+        for mid in [test_proto_mid, test_proto_with_arrays_mid]:
+            del data.proto_data_structure[mid]
+            assert(mid not in data.list_known_protobufs())
     return _dt
 
 
 @pytest.fixture
-def fake_msg():
+def fake_msg(test_proto_mid):
     def _fake_msg():
-        test = load_proto("fakemodelid_100_proto")
+        test = load_proto(test_proto_mid)
         """
         message Data1 {
         double a = 1;
@@ -132,9 +71,9 @@ def fake_msg():
 
 
 @pytest.fixture
-def fake_msg_with_arrays():
+def fake_msg_with_arrays(test_proto_with_arrays_mid):
     def _fake_msg_with_arrays():
-        test = load_proto("fakemodelidwitharrays_100_proto")
+        test = load_proto(test_proto_with_arrays_mid)
         m = test.ImageTagSet(image=[1, 2, 3], tag=["fish", "cat", "dog"], score=[0.8, 0.9, 1.0])
         msgb = m.SerializeToString()
         return msgb
@@ -167,3 +106,97 @@ def fake_msg_as_jsonwb():
                 'apv_model_as_string': '{\n    "a": 1.1111111111111111e+24,\n    "b": 666.666015625,\n    "c": 777,\n    "d": 77777777777777,\n    "e": true,\n    "f": "helives",\n    "g": "<RAW BYTES>"\n}',
                 'apv_sequence_number': 1}
     return _fake_msg_as_jsonwb
+
+
+@pytest.fixture
+def test_proto_url():
+    return "http://myserver.com/fakemodelid/1.0.0/fakemodelid-1.0.0-proto"
+
+
+@pytest.fixture
+def test_proto_mid():
+    return "http___myserver_com_fakemodelid_1_0_0_fakemodelid_1_0_0_proto"
+
+
+# Message name defined in proto below
+@pytest.fixture
+def test_proto_msg():
+    return "Data1"
+
+
+@pytest.fixture
+def test_proto_txt():
+    return """
+        syntax = "proto3";
+        package mygreatpackage;
+        message Data1 {
+            double a = 1;
+            float b = 2;
+            int32 c = 3;
+            int64 d = 4;
+            bool e = 5;
+            string f = 6;
+            bytes g = 7;
+        }
+        message Data2 {
+            Data1 a = 1;
+            map<string, Data1> b = 2;
+            map<string, int32> c = 3;
+            repeated Data1 d = 4;
+            repeated int32 e = 5;
+        }
+    """
+
+
+@pytest.fixture
+def test_proto_with_arrays_url():
+    return "http://myserver.com/fakemodelidwitharrays/1.0.0/fakemodelidwitharrays-1.0.0-proto"
+
+
+@pytest.fixture
+def test_proto_with_arrays_mid():
+    return "http___myserver_com_fakemodelidwitharrays_1_0_0_fakemodelidwitharrays_1_0_0_proto"
+
+
+# Message name defined in proto below
+@pytest.fixture
+def test_proto_with_arrays_msg():
+    return "ImageTagSet"
+
+
+@pytest.fixture
+def test_proto_with_arrays_txt():
+    return """
+    syntax = "proto3";
+    package YKhGXjKWHYsPwKJFfEPnmoHOkDkPKBxX;
+    service Model {
+      rpc classify (ImageTagSet) returns (ImageTagSet);
+    }
+    message ImageTagSet {
+      repeated int64 image = 1;
+      repeated string tag = 2;
+      repeated double score = 3;
+    }
+    """
+
+
+@pytest.fixture
+def test_probe_fake_schema_url():
+    return "http://myserver.com/probe_testschema/1.0.0/probe_testschema-1.0.0"
+
+
+@pytest.fixture
+def test_probe_fake_schema_txt():
+    return """
+    {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "properties": {
+          "value": {
+            "type": "number",
+            "description": ""
+          }
+        },
+        "required": ["value"],
+        "type": "object"
+      }
+    """
