a91c89611ebe00b221412184a441d09e4bdcfac5
Tommy Carpenter <tommy@research.att.com>
2018-11-02 11:12:06 -0400

    Upgrade to python3.7
    This is a maintanance commit and is not needed for a specific release.
    
    Change-Id: Ifca5862c1f6fe25b467b24e28064888e06f6825f
    Signed-off-by: Tommy Carpenter <tommy@research.att.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='acumos_proto_viewer',
-    version='1.5.7',  # REPEATED IN pom.xml MUST MATCH
+    version='1.6.0',  # REPEATED IN pom.xml MUST MATCH
     packages=find_packages(),
     author="Tommy Carpenter",
     author_email="tommy@research.att.com",
@@ -11,7 +11,11 @@ setup(
     keywords="",
     url="https://gerrit.acumos.org/r/#/admin/projects/proto-viewer",
     zip_safe=False,
-    install_requires=["requests", "jsonschema"],
+    install_requires=["requests >2.0.0, <3.0.0",
+                      "jsonschema >2.0.0, <3.0.0",
+                      "tornado >4.0.0, <5.0.0",
+                      "bokeh >1.0.0, <3.0.0",
+                      "redis >2.0.0, <3.0.0"],
     scripts=[
         "bin/fake_data.py",
         "bin/run.py"
